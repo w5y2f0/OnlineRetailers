915e4f666fc9f40ea1b93fbb4ca2d3c62d5e1aba
mi <h1272868064@163.com>
2019-04-29 16:27:19 +0800

    增加实体类

---
+++ api/src/main/java/com/mr/good/pojo/Goods.java
@@ -0,0 +1,105 @@
+package com.mr.good.pojo;
+
+import java.util.Date;
+
+public class Goods {
+    private Integer goId;
+
+    private String goName;
+
+    private String goSynopsis;
+
+    private Integer goType;
+
+    private Integer goStates;
+
+    private Date goDatetimes;
+
+    private Integer goSales;
+
+    private Double goOld;
+
+    private Double goNew;
+
+    private Integer goNum;
+
+    public Integer getGoId() {
+        return goId;
+    }
+
+    public void setGoId(Integer goId) {
+        this.goId = goId;
+    }
+
+    public String getGoName() {
+        return goName;
+    }
+
+    public void setGoName(String goName) {
+        this.goName = goName == null ? null : goName.trim();
+    }
+
+    public String getGoSynopsis() {
+        return goSynopsis;
+    }
+
+    public void setGoSynopsis(String goSynopsis) {
+        this.goSynopsis = goSynopsis == null ? null : goSynopsis.trim();
+    }
+
+    public Integer getGoType() {
+        return goType;
+    }
+
+    public void setGoType(Integer goType) {
+        this.goType = goType;
+    }
+
+    public Integer getGoStates() {
+        return goStates;
+    }
+
+    public void setGoStates(Integer goStates) {
+        this.goStates = goStates;
+    }
+
+    public Date getGoDatetimes() {
+        return goDatetimes;
+    }
+
+    public void setGoDatetimes(Date goDatetimes) {
+        this.goDatetimes = goDatetimes;
+    }
+
+    public Integer getGoSales() {
+        return goSales;
+    }
+
+    public void setGoSales(Integer goSales) {
+        this.goSales = goSales;
+    }
+
+    public Double getGoOld() {
+        return goOld;
+    }
+
+    public void setGoOld(Double goOld) {
+        this.goOld = goOld;
+    }
+
+    public Double getGoNew() {
+        return goNew;
+    }
+
+    public void setGoNew(Double goNew) {
+        this.goNew = goNew;
+    }
+
+    public Integer getGoNum() {
+        return goNum;
+    }
+
+    public void setGoNum(Integer goNum) {
+        this.goNum = goNum;
+    }
+}
