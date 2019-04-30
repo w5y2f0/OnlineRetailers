f47875dea3293a65083817700e6b432cadf654b1
mi <h1272868064@163.com>
2019-04-30 10:02:19 +0800

    完成优惠券模块中的增删改查

--- boot_backstage/src/main/java/com/mr/coupon/controller/CouponController.java
+++ boot_backstage/src/main/java/com/mr/coupon/controller/CouponController.java
@@ -1,7 +1,50 @@
 package com.mr.coupon.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.mr.coupon.service.CouponService;
+import com.mr.pojo.Coupon;
+import com.mr.utils.Page;
+import com.mr.utils.ResultVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.ws.rs.GET;
+import java.util.List;
+
 /**
  * Created by 黄圣博 on 2019/4/29.
  */
-public class CouponController  {
+@RestController
+@RequestMapping("coupon")
+public class CouponController {
+    @Autowired
+    private CouponService couponService;
+@GetMapping("selectAll")
+    public List<Coupon> selectAll(Page page){
+        List<Coupon> coupons = couponService.selectAll(page);
+        return coupons;
+    }
+    @PostMapping("insertCoupon")
+    public ResultVo insertCoupon(Coupon coupon){
+        ResultVo resultVo = couponService.insertCoupon(coupon);
+        return resultVo;
+    }
+    @PostMapping("deleteCoupon")
+    public ResultVo deleteCoupon(String id){
+        ResultVo resultVo = couponService.deleteCoupon(id);
+        return resultVo;
+    }
+    @GetMapping("selectById")
+    public Coupon selectById(String id){
+        Coupon coupon = couponService.selectById(id);
+        return coupon;
+    }
+    @PostMapping("updateCoupon")
+    public ResultVo updateCoupon(Coupon coupon){
+        ResultVo resultVo = couponService.updateCoupon(coupon);
+        return resultVo;
+    }
 }
