b666173528eb518841d1ecb3e0dc57e3328a2efa
mi <h1272868064@163.com>
2019-04-30 10:28:09 +0800

    完成商品参数增删查改

--- boot_backstage/src/main/java/com/mr/parameter/controller/ParameterController.java
+++ boot_backstage/src/main/java/com/mr/parameter/controller/ParameterController.java
@@ -1,7 +1,52 @@
 package com.mr.parameter.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.mr.parameter.service.ParameterService;
+import com.mr.picture.service.PictureService;
+import com.mr.pojo.Parameter;
+import com.mr.utils.Page;
+import com.mr.utils.ResultVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.ws.rs.POST;
+import java.util.List;
+
 /**
  * Created by 黄圣博 on 2019/4/29.
  */
+@RestController
+@RequestMapping("parameter")
 public class ParameterController {
+    @Autowired
+    private ParameterService parameterService;
+@GetMapping("selectAll")
+    public List<Parameter> selectAll(Page page){
+        PageHelper.startPage(page.getPage(),page.getLimit());
+        List<Parameter> parameter = parameterService.selectAll(page);
+        return parameter;
+    }
+    @PostMapping("insertParameter")
+    public ResultVo insertParameter(Parameter parameter){
+        ResultVo resultVo = parameterService.insertParameter(parameter);
+        return resultVo;
+    }
+    @PostMapping("deleteParameter")
+    public ResultVo deleteParameter(String id){
+        ResultVo resultVo = parameterService.deleteParameter(id);
+        return resultVo;
+    }
+    @GetMapping("selectById")
+    public Parameter selectById(String id){
+        Parameter parameter = parameterService.selectById(id);
+        return parameter;
+    }
+    @PostMapping("updateParameter")
+    public ResultVo updateParameter(Parameter parameter){
+        ResultVo resultVo = parameterService.updateParameter(parameter);
+        return resultVo;
+    }
 }
--- boot_backstage/src/main/java/com/mr/parameter/service/impl/ParameterServiceImpl.java
+++ boot_backstage/src/main/java/com/mr/parameter/service/impl/ParameterServiceImpl.java
@@ -1,11 +1,55 @@
 package com.mr.parameter.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.mr.parameter.mapper.ParameterMapper;
 import com.mr.parameter.service.ParameterService;
+import com.mr.pojo.Parameter;
+import com.mr.utils.Page;
+import com.mr.utils.ResultVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by 黄圣博 on 2019/4/29.
  */
 @Service
 public class ParameterServiceImpl implements ParameterService {
+    @Autowired
+    private ParameterMapper parameterMapper;
+
+    public List<Parameter> selectAll(Page page){
+        PageHelper.startPage(page.getPage(),page.getLimit());
+        List<Parameter> parameter = parameterMapper.selectAll();
+        return parameter;
+    }
+    public ResultVo insertParameter(Parameter parameter){
+        int insert = parameterMapper.insert(parameter);
+        if(insert>0){
+            return ResultVo.success("添加成功");
+        }else{
+            return ResultVo.error(500,"添加失败");
+        }
+    }
+    public ResultVo deleteParameter(String id){
+        int i = parameterMapper.deleteByPrimaryKey(Integer.parseInt(id));
+        if (i>0){
+            return ResultVo.success("删除成功");
+        }else{
+            return ResultVo.error(500,"删除错误");
+        }
+    }
+    public Parameter selectById(String id){
+        Parameter parameter = parameterMapper.selectByPrimaryKey(Integer.parseInt(id));
+        return parameter;
+    }
+    public ResultVo updateParameter(Parameter parameter){
+        int i = parameterMapper.updateByPrimaryKey(parameter);
+        if(i>0){
+            return ResultVo.success("修改成功");
+        }else{
+            return ResultVo.error(500,"修改失败");
+        }
+    }
 }
