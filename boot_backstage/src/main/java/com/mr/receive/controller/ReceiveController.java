9047efa45cbdf680bdfd4f639de185294ee5f7e3
mi <h1272868064@163.com>
2019-05-05 09:21:36 +0800

    完善receive模块增删查改方法

--- boot_backstage/src/main/java/com/mr/receive/controller/ReceiveController.java
+++ boot_backstage/src/main/java/com/mr/receive/controller/ReceiveController.java
@@ -1,10 +1,16 @@
 package com.mr.receive.controller;
 
+import com.mr.receive.service.ReceiveService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by 黄圣博 on 2019/4/29.
  */
 @RestController
+@RequestMapping("receive")
 public class ReceiveController {
+    @Autowired
+    private ReceiveService receiveService;
 }
--- boot_backstage/src/main/java/com/mr/receive/service/ReceiveService.java
+++ boot_backstage/src/main/java/com/mr/receive/service/ReceiveService.java
@@ -1,7 +1,20 @@
 package com.mr.receive.service;
 
+import com.github.pagehelper.PageHelper;
+import com.mr.pojo.Receive;
+import com.mr.utils.Page;
+import com.mr.utils.ResultVo;
+
+import java.util.List;
+
 /**
  * Created by 黄圣博 on 2019/4/29.
  */
 public interface ReceiveService {
+
+    public List<Receive> selectAll(Page page);
+    public ResultVo insertReceive(Receive parameter);
+    public ResultVo deleteReceive(String id);
+    public Receive selectById(String id);
+    public ResultVo updatePicture(Receive parameter);
 }
