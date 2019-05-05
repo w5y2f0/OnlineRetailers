42e00d110976507d49a5cf5c6bb2425044d4762a
fan <13994959519@163.com>
2019-05-05 11:06:07 +0800

    增加注解

--- boot_reception/src/main/java/com/mr/BootReceptionApplication.java
+++ boot_reception/src/main/java/com/mr/BootReceptionApplication.java
@@ -6,9 +6,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class BootReceptionApplication {
 
 	public static void main(String[] args) {
