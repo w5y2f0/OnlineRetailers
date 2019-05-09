8f5889a472c68a6bcce89d65905a1a5e0b58516f
fan <13994959519@163.com>
2019-05-09 10:14:33 +0800

    优惠卷vo

---
+++ boot_api/src/main/java/com/mr/pojo/CouponVo.java
@@ -0,0 +1,86 @@
+package com.mr.pojo;
+
+import java.util.Date;
+
+/**
+ * Created by Fan on 2019/5/9.
+ */
+public class CouponVo {
+
+    private Integer coId;//卷编号
+    private Double coMoney;//价值
+    private Double coLimit;//限制价格
+    private Date coValid;//有效时间
+    private Date reTime;//领取时间
+    private Integer reUse;//使用状态
+    private Integer reTerm;//过期状态
+
+    @Override
+    public String toString() {
+        return "CouponVo{" +
+                "coId=" + coId +
+                ", coMoney=" + coMoney +
+                ", coLimit=" + coLimit +
+                ", coValid=" + coValid +
+                ", reTime=" + reTime +
+                ", reUse=" + reUse +
+                ", reTerm=" + reTerm +
+                '}';
+    }
+
+    public Integer getCoId() {
+        return coId;
+    }
+
+    public void setCoId(Integer coId) {
+        this.coId = coId;
+    }
+
+    public Double getCoMoney() {
+        return coMoney;
+    }
+
+    public void setCoMoney(Double coMoney) {
+        this.coMoney = coMoney;
+    }
+
+    public Double getCoLimit() {
+        return coLimit;
+    }
+
+    public void setCoLimit(Double coLimit) {
+        this.coLimit = coLimit;
+    }
+
+    public Date getCoValid() {
+        return coValid;
+    }
+
+    public void setCoValid(Date coValid) {
+        this.coValid = coValid;
+    }
+
+    public Date getReTime() {
+        return reTime;
+    }
+
+    public void setReTime(Date reTime) {
+        this.reTime = reTime;
+    }
+
+    public Integer getReUse() {
+        return reUse;
+    }
+
+    public void setReUse(Integer reUse) {
+        this.reUse = reUse;
+    }
+
+    public Integer getReTerm() {
+        return reTerm;
+    }
+
+    public void setReTerm(Integer reTerm) {
+        this.reTerm = reTerm;
+    }
+}
